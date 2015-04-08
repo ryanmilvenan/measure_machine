cec12551a3709b3e9b791f4e04716768768ec4d3
Ryan Milvenan <ryan@warballoon.com>
2015-04-08 15:38:42 -0600

    Updated to FSM

--- sketch_apr05a/sketch_apr05a.ino
+++ sketch_apr05a/sketch_apr05a.ino
@@ -1,6 +1,10 @@
+#include <Button.h>
 #include <FiniteStateMachine.h>
-#include <Bounce2.h>
 #include <LiquidCrystal.h>
+
+const byte NUM_LIQUIDS = 2;
+const byte NUM_SOLIDS = 2;
+const byte NUM_MEASUREMENTS = 2;
 
 //I/O
 LiquidCrystal lcd(13,12,11,10,9,8);
@@ -9,127 +13,385 @@ int pumpControlPin = 7;
 
 //Control Buttons
 int backButtonPin = 0;
-Bounce backButton = Bounce();
+Button backButton = Button(backButtonPin, PULLUP);
 
 int forwardButtonPin = 1;
-Bounce forwardButton = Bounce();
+Button forwardButton = Button(forwardButtonPin, PULLUP);
 
 int acknowledgeButtonPin = 2;
-Bounce acknowledgeButton = Bounce();
+Button ackButton = Button(acknowledgeButtonPin, PULLUP);
 
 int activateButtonPin = 3;
-Bounce activateButton = Bounce();
+Button activateButton = Button(activateButtonPin, PULLUP);
 
 int cancelButtonPin = 4;
-Bounce cancelButton = Bounce();
+Button cancelButton = Button(cancelButtonPin, PULLUP);
 
-int dbInterval = 10;
+//Finite State Machine 
+State Nav = State(empty, navUpdate, cleanDisplay);
 
-State Nav = State(empty, navUpdate, cleanUp);
-State Liquid = State(liquidUpdate);
-State Solid = State(solidUpdate);
+State LiquidDispenseSelect = State(cleanBottomLine, liquidDispenseSelectUpdate, empty);
+State LiquidMeasurementSelect = State(empty, liquidMeasurementSelectUpdate, cleanDisplay);
+State LiquidAmountSelect = State(empty, liquidAmountSelectUpdate, cleanDisplay);
+State LiquidStart = State(empty, liquidStartUpdate, empty);
+
+State SolidDispenseSelect = State(cleanBottomLine, solidDispenseSelectUpdate, empty);
+State SolidMeasurementSelect = State(empty, solidMeasurementSelectUpdate, cleanDisplay);
 
 FSM measureMachine = FSM(Nav);
 
-bool back = false;
-bool forward = false;
-bool acknowledged = false;
-String dispense;
-String measurement;
-double amount = 0.0;
+char liquids[2][18] = {"<-    Water   ->",
+                       "<-  OliveOil  ->"};
+
+char solids[2][18] = {"<-   Parsley  ->",
+                      "<-  Cinnamon  ->"};
+                      
+char measurements[2][18] = {"<-    Cups    ->",
+                            "<-    Tbls    ->"};
+                            
+char tbls[6][18] = {"    1/8 tsp   ->",
+                    "<-  1/4 tsp   ->",
+                    "<-  1/2 tsp   ->",
+                    "<-  1 tsp     ->",
+                    "<-  1/2 tbls  ->",
+                    "<-  1 tbls    ->"};
+
+double tblsEquiv[6] = {0.41,
+                          0.83,
+                          0.16,
+                          0.33,
+                          0.5,
+                          1};
+                          
+int liquidIdx = 0;
+int solidIdx = 0;
+int liqMeasureIdx = 0;
+int tblsIdx = 0;
+int liqStartTime;
+double liqEndTime;
+int solStartTime;
+double solEndTime;
+double liqAmount = 0.0;
+double solAmount = 0.0;
+bool liquidActive = false;
+bool solidActive = false;
+bool pumping = false;
+
 
 void setup() {
   Serial.begin(9600);
-  //I/O Initialization
   pinMode(pumpControlPin, OUTPUT);
   //pinMode(scaleVoltagePin, INPUT);
-  
-  //Button Initialization
-  pinMode(backButtonPin, INPUT_PULLUP);
-  pinMode(forwardButtonPin, INPUT_PULLUP);
-  pinMode(acknowledgeButtonPin, INPUT_PULLUP);
-  pinMode(activateButtonPin, INPUT_PULLUP);
-  pinMode(cancelButtonPin, INPUT_PULLUP);
-  
-  backButton.attach(backButtonPin);
-  forwardButton.attach(forwardButtonPin);
-  acknowledgeButton.attach(acknowledgeButtonPin);
-  activateButton.attach(activateButtonPin);
-  cancelButton.attach(cancelButtonPin);
-  
-  backButton.interval(dbInterval);
-  forwardButton.interval(dbInterval);
-  acknowledgeButton.interval(dbInterval);
-  activateButton.interval(dbInterval);
-  cancelButton.interval(dbInterval);
 
   //LCD Initialization 
   lcd.begin(16, 2);
   lcd.clear();
-  
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  dispenseLiquid();
+  dispenseSolid();
   measureMachine.update();
 }
 
 //Utility Functions
 void navUpdate() {
-  if(back) {
-    measureMachine.transitionTo(Liquid);
+  if(backButton.uniquePress()) {
+    measureMachine.transitionTo(LiquidDispenseSelect);
   }
-  if(forward) {
-    measureMachine.transitionTo(Solid);
+  if(forwardButton.uniquePress()) {
+    measureMachine.transitionTo(SolidDispenseSelect);
   }
   lcd.setCursor(0,0);
   lcd.print(" measureMachine");
   lcd.setCursor(0,1);
   lcd.print("<-Liquid Solid->");
-  checkBackNav();
-  checkForwardNav();
-}
-
-void checkBackNav() {
-  int backValue = backButton.read();
-  if(backValue == LOW) {
-    Serial.println("Got Called");
-    back = true;
-  }
-    
-}
-
-void checkForwardNav() {
-  int forwardValue = forwardButton.read();
-  if(forwardValue == LOW)
-    forward = true;
 }
 
 void empty() {
 }
 
-void cleanUp() {
-  back = false;
-  forward = false;
-  acknowledged = false;
-}
-
 void reset() {
-  back = false;
-  forward = false;
-  acknowledged = false;
-  dispense = "";
-  measurement = "";
-  amount = 0.0;
+  liqAmount = 0.0;
+  solAmount = 0.0;
+  liquidIdx = 0;
+  solidIdx = 0;
+  liqMeasureIdx = 0;
+  cleanDisplay();
 }
 
-void liquidUpdate() {
+/***********************************
+*              LIQUID          
+***********************************/
+
+/////////LIQUID DISPENSE SELECT
+void liquidDispenseSelectUpdate() {
+  if(cancelButton.uniquePress()) {
+    reset();
+    measureMachine.transitionTo(Nav);
+  }
+  
+  if(backButton.uniquePress()) {
+    liquidIdx = --liquidIdx % NUM_LIQUIDS;
+  }
+  
+  if(forwardButton.uniquePress()) {
+    liquidIdx = ++liquidIdx % NUM_LIQUIDS;
+  }
+  
+  if(ackButton.uniquePress()) {
+    measureMachine.transitionTo(LiquidMeasurementSelect);
+  }
+ 
   lcd.setCursor(0,0);
-  lcd.print("liquid");
+  lcd.print(liquids[liquidIdx]);
+}
+
+
+//////////LIQUID MEASUREMENT SELECT
+void liquidMeasurementSelectUpdate() {
+  if(cancelButton.uniquePress()) {
+    reset();
+    measureMachine.transitionTo(Nav);
+  }
+  
+  if(backButton.uniquePress()) {
+    liqMeasureIdx = --liqMeasureIdx % NUM_MEASUREMENTS;
+  }
+  
+  if(forwardButton.uniquePress()) {
+    liqMeasureIdx = ++liqMeasureIdx % NUM_MEASUREMENTS;
+  }
+  
+  if(ackButton.uniquePress()) {
+    measureMachine.transitionTo(LiquidAmountSelect);
+  }
+  
+  lcd.setCursor(0,1);
+  lcd.print(measurements[liqMeasureIdx]);
+}
+
+//////////AMOUNT SELECT
+void liquidAmountSelectUpdate() {
+  if(cancelButton.uniquePress()) {
+    reset();
+    measureMachine.transitionTo(Nav);
+  }
+  
+  if(ackButton.uniquePress()) {
+    measureMachine.transitionTo(LiquidStart);
+  }
+  
+  lcd.setCursor(0,0);
+  lcd.print("How much?");
+  
+  switch(liqMeasureIdx) {
+    //Cups
+    case 0:
+      if(backButton.uniquePress()) {
+        if(liqAmount > 0) {
+          liqAmount -= 0.25;
+        }
+      }
+      
+      if(forwardButton.uniquePress()) {
+        if(liqAmount < 2) {
+          liqAmount += 0.25;
+        }
+      }
+      
+      lcd.setCursor(0,1);
+      lcd.print(liqAmount);
+    break;
+    
+    //Spoons
+    case 1:
+      if(backButton.uniquePress()) {
+        if(liqAmount > 0) {
+          liqAmount -= 1;
+        }
+      }
+      
+      if(forwardButton.uniquePress()) {
+        if(liqAmount < 6) {
+          liqAmount += 1;
+        }
+      }
+      lcd.setCursor(0,1);
+      lcd.print(tbls[(int)liqAmount]);
+    break;
+  }
+}
+
+//////////AMOUNT SELECT
+void liquidStartUpdate() {
+  if(cancelButton.uniquePress()) {
+    reset();
+    measureMachine.transitionTo(Nav);
+  }
+  
+  if(activateButton.uniquePress()) {
+    liquidActive = true;
+    measureMachine.transitionTo(Nav);
+  }
+  
+  lcd.setCursor(0,0);
+  lcd.print("Press Green Btn ");
+  lcd.setCursor(0,1);
+  lcd.print("to begin"); 
+}
+
+/***********************************
+*              SOLID          
+***********************************/
+
+/////////SOLID DISPENSE SELECT
+void solidDispenseSelectUpdate() {
+  if(cancelButton.uniquePress()) {
+    reset();
+    measureMachine.transitionTo(Nav);
+  }
+  
+  if(backButton.uniquePress()) {
+    solidIdx = --solidIdx % NUM_SOLIDS;
+  }
+  
+  if(forwardButton.uniquePress()) {
+    solidIdx = ++solidIdx % NUM_SOLIDS;
+  }
+  
+  if(ackButton.uniquePress()) {
+    measureMachine.transitionTo(SolidMeasurementSelect);
+  }
+ 
+  lcd.setCursor(0,0);
+  lcd.print(solids[solidIdx]);
+}
+
+
+//////////SOLID MEASUREMENT SELECT
+void solidMeasurementSelectUpdate() {
+  if(cancelButton.uniquePress()) {
+    reset();
+    measureMachine.transitionTo(Nav);
+  }
+  
+  if(backButton.uniquePress()) {
+    //measureIdx = --measureIdx % NUM_MEASUREMENTS;
+  }
+  
+  if(forwardButton.uniquePress()) {
+    //measureIdx = ++measureIdx % NUM_MEASUREMENTS;
+  }
+  
+  lcd.setCursor(0,1);
+  lcd.print(measurements[1]);
+}
+
+
+/***********************************
+*              UTILITY          
+***********************************/
+void cleanBottomLine() {
+  lcd.setCursor(0,1);
+  lcd.print("               ");
+}
+
+void cleanDisplay() {
+  lcd.clear();
 }
 
 void solidUpdate() {
   lcd.setCursor(0,0);
   lcd.print("solid");
 }
+
+/***********************************
+*            DISPENSING          
+***********************************/
+
+void dispenseLiquid() {
+  double tbls;
+  switch(liqMeasureIdx) {
+    //Cups
+    case 0:
+      beginLiquidDispenseCups(liqAmount);
+    break;
+    
+    //Spoons
+    case 1:
+      tbls = tblsEquiv[(int)liqAmount];
+      beginLiquidDispenseSpoons(tbls);
+    break;
+  }
+}
+
+void dispenseSolid() {
+  beginSolidDispenseSpoons(solAmount);
+}
+
+void beginLiquidDispenseCups(double amountIn) {
+  if(liquidActive) {
+    double dispenseAmount = amountIn;
+    if(!pumping) {
+      startTimingLiquid();
+      calculateEndTimeLiquidCups(dispenseAmount);
+      digitalWrite(pumpControlPin, HIGH);
+    } else {
+      if(millis() - liqStartTime >= liqEndTime) {
+        pumping = false;
+        digitalWrite(pumpControlPin, LOW);
+        liquidActive = false;
+      }
+    }
+  } else {
+    digitalWrite(pumpControlPin, LOW);
+  }
+}
+
+void beginLiquidDispenseSpoons(double amountIn) {
+  if(liquidActive) {
+    double dispenseAmount = amountIn;
+    if(!pumping) {
+      startTimingLiquid();
+      calculateEndTimeLiquidSpoons(dispenseAmount);
+      digitalWrite(pumpControlPin, HIGH);
+    } else {
+      if(millis() - liqStartTime >= liqEndTime) {
+        pumping = false;
+        digitalWrite(pumpControlPin, LOW);
+        liquidActive = false;
+      }
+    }
+  } else {
+    digitalWrite(pumpControlPin, LOW);
+  }
+}
+
+void beginSolidDispenseSpoons(double amountIn) {
+    if(solidActive) {
+      double dispenseAmount = amountIn;
+    }
+}
+
+void startTimingLiquid() {
+  liqStartTime = millis();
+  pumping = true;
+}
+
+void calculateEndTimeLiquidCups(double amountIn) {
+  double duration = (amountIn * 237)/(1.21);
+  Serial.println(duration);
+  duration = duration * 1000;
+  liqEndTime = duration;
+  Serial.println(liqEndTime);
+}
+
+void calculateEndTimeLiquidSpoons(double amountIn) {
+  double duration = (amountIn * 14)/(1.21);
+  duration = duration * 1000;
+  Serial.println(duration);
+  liqEndTime = duration;
+  Serial.println(liqEndTime);
+}
+
